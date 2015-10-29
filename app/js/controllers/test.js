c4713ec26290c888dccab8b31fbb5db09a1eb18b
Ronald91 <>
2015-10-29 17:36:44 -0400

    Figured out how to do routing and new controllers with this setup

---
+++ app/js/controllers/test.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var controllersModule = require('./_index');
+
+/**
+ * @ngInject
+ */
+function TestCtrl() {
+
+  // ViewModel
+  var vm = this;
+
+  vm.title = 'I am a test!';
+
+}
+
+controllersModule.controller('TestCtrl', TestCtrl);
--- app/js/on_config.js
+++ app/js/on_config.js
@@ -5,17 +5,28 @@
  */
 function OnConfig($stateProvider, $locationProvider, $urlRouterProvider) {
 
-  $locationProvider.html5Mode(true);
+    $locationProvider.html5Mode(true);
+    var home = {
+        name: 'Home',
+        url: '/',
+        controller: 'ExampleCtrl as home',
+        templateUrl: 'home.html',
+        title: 'Home'
+    };
+    
+    var test= {
+        name: 'Test',
+        url: '/test',
+        controller: 'TestCtrl as test',
+        templateUrl: 'test.html',
+        title: 'Test'
+    };
 
-  $stateProvider
-  .state('Home', {
-    url: '/',
-    controller: 'ExampleCtrl as home',
-    templateUrl: 'home.html',
-    title: 'Home'
-  });
+    $stateProvider.state(home);
+    $stateProvider.state(test);
 
-  $urlRouterProvider.otherwise('/');
+
+    $urlRouterProvider.otherwise('/');
 
 }
 
