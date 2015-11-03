977499da7beb38c86c4d365424ba24833443d282
LaToya <LaToya@LLRich30032.attlocal.net>
2015-11-03 17:04:59 -0500

    Added logic for FAQ

--- app/js/controllers/about_us.js
+++ app/js/controllers/about_us.js
@@ -9,6 +9,60 @@ function AboutUsCtrl() {
 
     // ViewModel
     var vm = this;
+    vm.quesOneColl= {
+        name: "one", 
+        value: true
+    };
+    vm.quesTwoColl={
+        name: "two", 
+        value: true
+    };
+    vm.quesThreeColl= {
+        name: "three", 
+        value: true
+    };
+    vm.quesFourColl={
+        name: "four", 
+        value: true
+    };
+    vm.quesFiveColl= {
+        name: "five", 
+        value: true
+    };
+    vm.quesSixColl={
+        name: "six", 
+        value: true
+    };
+    vm.quesSevenColl={
+        name: "seven", 
+        value: true
+    };
+    
+    vm.isQuesColl=function(object){
+      switch (object.name){
+          case "one":
+              vm.quesOneColl.value=!vm.quesOneColl.value;
+              break;
+          case "two":
+              vm.quesTwoColl.value=!vm.quesTwoColl.value;
+              break;
+          case "three":
+              vm.quesThreeColl.value=!vm.quesThreeColl.value;
+              break;
+          case "four":
+              vm.quesFourColl.value=!vm.quesFourColl.value;
+              break;
+          case "five":
+              vm.quesFiveColl.value=!vm.quesFiveColl.value;
+              break;
+          case "six":
+              vm.quesSixColl.value=!vm.quesSixColl.value;
+              break;
+          case "seven":
+              vm.quesSevenColl.value=!vm.quesSevenColl.value;
+              break;
+      }
+    };
 
 }
 
